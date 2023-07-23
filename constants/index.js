a1f64259b0379c8833eb91f4fc328cd9f28c4512
HESSAM COIN <105963287+hessamcoin-new@users.noreply.github.com>
2023-07-23 16:51:21 +0000

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xc7d825eead5d09504ec6636b3546a0278ca36347";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
